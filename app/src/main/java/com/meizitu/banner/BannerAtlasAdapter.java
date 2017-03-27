3deb5abae2c490969963327a568df3eef4351c9f
cgpllx <cgpllx1@qq.com>
2017-03-27 23:48:29 +0800

    去掉了不要的类

--- app/src/main/java/com/meizitu/banner/BannerAtlasAdapter.java
+++ app/src/main/java/com/meizitu/banner/BannerAtlasAdapter.java
@@ -33,26 +33,14 @@ public class BannerAtlasAdapter<T extends IQfangBanner> extends AbstractViewPage
     public View newView(ViewGroup container, int position) {
         View contentView = LayoutInflater.from(container.getContext()).inflate(R.layout.viewpager_eachpage_layout, null);
         PhotoView imageView = EasyViewUtil.findViewById(contentView, R.id.photoview);
-        final EasyProgressFrameLayout progressLayout = EasyViewUtil.findViewById(contentView, R.id.progressView);
-        progressLayout.showLoadingView();
+//        final EasyProgressFrameLayout progressLayout = EasyViewUtil.findViewById(contentView, R.id.progressView);
+//        progressLayout.showLoadingView();
         IQfangBanner qfangBanner = getItem(position);
         Glide.with(container.getContext())//
                 .load(qfangBanner.getImageUrl())//
                 .diskCacheStrategy(DiskCacheStrategy.ALL)//
-                .placeholder(R.drawable.ic_menu_camera)//
-                .dontAnimate().into(new GlideDrawableImageViewTarget(imageView) {
-            @Override
-            public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> animation) {
-                progressLayout.showContentView();
-                super.onResourceReady(resource, animation);
-            }
-
-            @Override
-            public void onLoadFailed(Exception e, Drawable errorDrawable) {
-                progressLayout.showErrorView();
-                super.onLoadFailed(e, errorDrawable);
-            }
-        });
+                .placeholder(R.drawable.image_detail_placeholder)//
+                .dontAnimate().into(imageView);
         return contentView;
     }
 }
