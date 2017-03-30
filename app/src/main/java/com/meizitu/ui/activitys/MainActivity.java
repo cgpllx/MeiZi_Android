660d5c309a98f561ee242103132966403f50ad40
cgpllx <cgpllx1@qq.com>
2017-03-30 18:24:01 +0800

    完善2

--- app/src/main/java/com/meizitu/ui/activitys/MainActivity.java
+++ app/src/main/java/com/meizitu/ui/activitys/MainActivity.java
@@ -1,17 +1,11 @@
 package com.meizitu.ui.activitys;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.meizitu.R;
 
@@ -22,16 +16,12 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-
+        initTitleBar();
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
-
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
@@ -47,26 +37,23 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
     }
 
 
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
+//        if (id == R.id.nav_camera) {
+//            // Handle the camera action
+//        } else if (id == R.id.nav_gallery) {
+//
+//         {
+//
+//        } else if (id == R.id.nav_share) {
+//
+//        } else if (id == R.id.nav_send) {
+//
+//        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
---
+++ app/src/main/java/com/meizitu/ui/fragments/IndexFragment.java
@@ -0,0 +1,77 @@
+package com.meizitu.ui.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.meizitu.R;
+import com.meizitu.adapter.GroupImageInfoListAdapter;
+import com.meizitu.mvp.contract.ImageListContract;
+import com.meizitu.mvp.presenter.ImageListPresenter;
+import com.meizitu.ui.items.Item_GroupImageInfoListItem;
+
+import javax.inject.Inject;
+
+import cc.easyandroid.easyrecyclerview.EasyFlexibleAdapter;
+
+
+public class IndexFragment extends BaseListFragment<Item_GroupImageInfoListItem> implements ImageListContract.View {
+
+    @Inject
+    ImageListPresenter presenter;
+
+    @Override
+    protected void onQfangViewCreated(View view, Bundle savedInstanceState) {
+        super.onQfangViewCreated(view, savedInstanceState);
+        setUserVisibleHint(true);
+        setHasOptionsMenu(true);
+//        getComponent(ImageListComponent.class).inject(this);
+        presenter.attachView(this);
+    }
+
+    public static Fragment newInstance() {
+        IndexFragment fragment = new IndexFragment();
+        return fragment;
+    }
+
+    @Override
+    protected EasyFlexibleAdapter<Item_GroupImageInfoListItem> onCreateEasyRecyclerAdapter() {
+        return new GroupImageInfoListAdapter();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.imagelistmenu, menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.listshare:
+                presenter.share(getActivity());
+                break;
+            case R.id.listfeedback:
+                presenter.feedback(getActivity());
+                break;
+            case R.id.listfavourablecomment:
+                presenter.favourablecomment(getContext());
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (presenter != null) {
+            presenter.detachView();
+            presenter = null;
+        }
+    }
+}
